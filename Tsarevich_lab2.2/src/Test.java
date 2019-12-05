c4065cc9683a43a51a5b1e01506e3c9219a86092
RookEndgame <55838011+RookEndgame@users.noreply.github.com>
2019-12-05 14:41:59 +0300

    Add files via upload

---
+++ Tsarevich_lab2.2/src/Test.java
@@ -0,0 +1,17 @@
+package ru.mirea.lab2;
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        double r;
+        ru.mirea.lab2.Circle k1 = new ru.mirea.lab2.Circle(3.1,4.1,5.1,"red");
+        System.out.println("Длина окружности = " + k1.getLength() + "см\n");
+        Scanner source = new Scanner(System.in);
+        System.out.println("Введите радиус ");
+        r = source.nextDouble();
+        k1.setR(r);
+        System.out.println("\nДлина окружности = " + k1.getLength() + "см");
+    }
+}
